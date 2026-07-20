0e4799684123638230b6f44a8e6c9212fa7a7d75
hamdi.hassan <hubeyda201717@gmail.com>
2026-07-20 20:40:38 +0100

    wip

--- infra/variables.tf
+++ infra/variables.tf
@@ -146,7 +146,6 @@ variable "alert_email" {
 variable "route53_zone_name" {
   description = "Existing Route 53 public hosted zone CraftHaven's storefront is delegated under, e.g. crafthaven.example"
   type        = string
-  default     = ""
 }
 
 variable "app_domain_name" {
