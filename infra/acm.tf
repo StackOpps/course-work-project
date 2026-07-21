3c2f3491be8cf5151b191970d4fad03f5e63535b
hamdi.hassan <hubeyda201717@gmail.com>
2026-07-21 20:43:27 +0100

    Deploy infra

--- infra/acm.tf
+++ infra/acm.tf
@@ -10,18 +10,10 @@ resource "aws_acm_certificate" "web" {
 }
 
 resource "aws_route53_record" "web_cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.web.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
   zone_id         = data.aws_route53_zone.main.zone_id
-  name            = each.value.name
-  type            = each.value.type
-  records         = [each.value.record]
+  name            = tolist(aws_acm_certificate.web.domain_validation_options)[0].resource_record_name
+  type            = tolist(aws_acm_certificate.web.domain_validation_options)[0].resource_record_type
+  records         = [tolist(aws_acm_certificate.web.domain_validation_options)[0].resource_record_value]
   ttl             = 60
   allow_overwrite = true
   depends_on      = [aws_acm_certificate.web]
@@ -29,5 +21,5 @@ resource "aws_route53_record" "web_cert_validation" {
 
 resource "aws_acm_certificate_validation" "web" {
   certificate_arn         = aws_acm_certificate.web.arn
-  validation_record_fqdns = [for record in aws_route53_record.web_cert_validation : record.fqdn]
+  validation_record_fqdns = [aws_route53_record.web_cert_validation.fqdn]
 }
