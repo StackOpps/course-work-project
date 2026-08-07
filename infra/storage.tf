a6f54a41a3d40041b2d7e689d8918074df08ce3c
hamdi.hassan <hubeyda201717@gmail.com>
2026-08-07 13:51:40 +0100

    Deploy infra

--- infra/storage.tf
+++ infra/storage.tf
@@ -1,3 +1,4 @@
+data "aws_region" "current" {}
 resource "aws_s3_bucket" "assets" {
   bucket = "${local.name}-assets-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.name}"
 
